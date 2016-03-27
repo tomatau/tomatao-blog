bd775be51741859c254a2da98059124e2e6d9bd3
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-03-27 20:45:52 +0100

    using tomatao-blog.herokuapp in disqus url

--- src/app/components/routes/BlogRoute.js
+++ src/app/components/routes/BlogRoute.js
@@ -68,7 +68,7 @@ class BlogRoute extends React.Component {
               shortname={'Tomatao Blog'}
               identifier={location.pathname}
               title={selectedPost.meta.title}
-              // url="http://www.example.com/example-thread"
+              url={`http://tomatao-blog.herokuapp.com/${location.pathname}`}
             />
           </div>
         )}
