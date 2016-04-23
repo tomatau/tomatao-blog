26284429a67911c68883534a68f182d50f616af6
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-04-23 21:38:10 +0100

    added opbeat and updated licence expression

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,10 +1,18 @@
 import 'config/environment'
 import '~/scripts/helpers/cssModulesHook'
+import opbeat from 'opbeat'
 import http from 'http'
 import serve from 'koa-static'
 import { ROOT, STATIC, SERVER, SOCKETS } from 'config/paths'
 import { isomorphicTools } from 'server/isomorphicTools'
 import app from 'server/index'
+
+opbeat.start({
+  appId: process.env.OPBEAT_APP_ID,
+  organizationId: process.env.OPBEAT_ORD_ID,
+  secretToken: process.env.OPBEAT_SECRET,
+  active: process.env.NODE_ENV === 'production',
+})
 
 const log = {
   app: debug('app'),
@@ -16,9 +24,12 @@ isomorphicTools.server(ROOT, () => {
   const { rootRouter, setRoutes } = require(`${SERVER}/router`)
   setRoutes(isomorphicTools.assets())
 
-  app.use(function *() {
-    yield rootRouter.routes()
+  rootRouter.use(function *(next) {
+    opbeat.setTransactionName(`${this.request.method} ${this._matchedRoute}`)
+    yield next
   })
+
+  app.use(rootRouter.routes())
 })
 
 const server = http.createServer(app.callback())
